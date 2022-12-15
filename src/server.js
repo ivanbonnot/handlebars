32294d288b996f99c6c5175d4a0520384dac9d44
Ivan Bonnot <ivanbonnot@gmail.com>
2022-12-15 20:58:43 -0300

    implementando hbs

--- src/server.js
+++ src/server.js
@@ -1,24 +1,37 @@
 const morgan = require('morgan');
 const express = require('express');
+const exphbs = require('express-handlebars')
+
+
 const app = express();
+
 
 //Settings
 app.set('port', process.env.PORT || 8080)
 app.set('json spaces', 2)
 
+
+//HBS
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
 //Middlewares
 app.use(morgan('dev'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('./public'))
+
 
 //Starting the server
 app.listen(app.get('port'), () => {
     console.log(`Server on port ${app.get('port')}`);
 });
 
+
 //Routes
-app.use('/api/productos',require('./routes/products'))
+app.use('/api/productos', require('./routes/products'))
 
 
 
